e0f944dfde08ec5302bb8acb0292364b9c762581
t-hayashi00 <ee57041@meiji.ac.jp>
2018-03-31 03:40:36 +0900

    バグ修正
    爆弾ブロックに接触すると常時ノックバックが発生するバグを修正

--- src/scene/ingame/actor/object/BombBlock.hx
+++ src/scene/ingame/actor/object/BombBlock.hx
@@ -7,7 +7,7 @@ package scene.ingame.actor.object;
 class BombBlock extends Object
 {
 	private var map:Map <Int, Block>;
-	
+
 	public override function new(x:Float, y:Float, map:Map<Int, Block>)
 	{
 		this.map = map;
@@ -15,7 +15,7 @@ class BombBlock extends Object
 		super(x, y, 16, 16);
 		HP = 1;
 	}
-	
+
 	public override function update():Bool
 	{
 		knockBack = 0;
@@ -37,10 +37,13 @@ class BombBlock extends Object
 		}
 		return false;
 	}
-	
+
 	public override function hitAffect(e:Actor):Void
 	{
-		e.knockBack = 6;
-		e.hitStop = 6;
+		if (e.isLimitBreak())
+		{
+			e.knockBack = 6;
+			e.hitStop = 6;
+		}
 	}
 }
